f8b201efe0dc61e27c37f46277b3431a7bc406e8
unknown <mydulcse62.niter@gmail.com>
2024-02-28 19:40:45 +0600

    added functtionality

--- js/hundingPhone.js
+++ js/hundingPhone.js
@@ -2,13 +2,12 @@ const loadPhoneDetails = async (searchItem,isShowAll) =>{
   const res =await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchItem}`);
   const data =await res.json();
   const phones = data.data;
-  console.log(phones)
   phoneCardAdd(phones,isShowAll)
+ 
 };
 
 loadPhoneDetails();
       let innerContent=[];
-      let count = 0;
 const phoneCardAdd =(phones,isShowAll)=>{
   const phonesCardsId = document.getElementById('phones_cards');
   const showALlId = document.getElementById('show-all-container');
@@ -32,11 +31,10 @@ const phoneCardAdd =(phones,isShowAll)=>{
       <p>${phone.slug}</p>
       <h3 class="text-2xl font-bold">$999</h3>
       <div class="card-actions">
-        <button onclick = "showDetails();" class="bg-[#0D6EFD] px-4 py-2 rounded-md text-white">Show Details</button>
+        <button onclick = "showDetails('${phone.slug}');" class="bg-[#0D6EFD] px-4 py-2 rounded-md text-white">Show Details</button>
       </div>
     </div>
   </div>`
-  
    });
 
    phonesCardsId.innerHTML = innerContent;
@@ -64,7 +62,46 @@ const loadingSpinner=(isloading)=>{
     spinner.classList.add('hidden');
   }
 }
-
-const showDetails = ()=>{
+let count = 0;
+const showDetails =async (id)=>{
+  const res =await fetch(`https://openapi.programming-hero.com/api/phone/${id}`);
+  const data =await res.json();
+  const phones = data.data;
+   console.log(phones)
+   count +=1;
+  
+  const modalBox = document.getElementById('Modal_container')
+  modalBox.innerHTML = `<div>
+  <div class="image flex justify-center  bg-[#0D6EFD0D]">
+    <img src="${phones.image}" alt="">
+  </div>
+  <div class="discription ">
+    <h3 class="font-bold text-2xl text-[#403F3F]">Iphone 13 Pro Max</h3>
+  <p class="py-4">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
+  <h3 class=" text-2xl text-[#403F3F]">Display Size : <span class=" text-xl text-[#706F6F]">${phones.mainFeatures.displaySize}</span></h3>
+  <h3 class=" text-2xl text-[#403F3F]">Chipset : <span class=" text-xl text-[#706F6F]">${phones.mainFeatures.chipSet}</span></h3>
+  <h3 class=" text-2xl text-[#403F3F]">Memory : <span class=" text-xl text-[#706F6F]">${phones.mainFeatures.memory}</span></h3>
+  <h3 class=" text-2xl text-[#403F3F]">Storage : <span class=" text-xl text-[#706F6F]">${phones.mainFeatures.storage}</span></h3>
+  <h3 class=" text-2xl text-[#403F3F]">Slug : <span class=" text-xl text-[#706F6F]">${phones.slug}</span></h3>
+  <h3 class=" text-2xl text-[#403F3F]">Release data : <span class=" text-xl text-[#706F6F]">${phones.releaseDate?phones.releaseDate : "Not available"}</span></h3>
+  <h3 class=" text-2xl text-[#403F3F]">Brand : <span class=" text-xl text-[#706F6F]">${phones.brand}</span></h3>
+  <h3 class=" text-2xl text-[#403F3F]">GPS :<span class=" text-xl text-[#706F6F]"> ${phones.others.GPS?phones.others.GPS : "Not available"}</span></h3>
+  </div>
+  <div class=" flex justify-center"><button onClick="cartCounter()" class=" bg-slate-800 px-6 py-4 text-white rounded-md">add cart</button></div>
+  <div class="modal-action">
+    <form method="dialog">
+      <!-- if there is a button in form, it will close the modal -->
+      <button class="btn">Close</button>
+    </form>
+  </div>
+</div>`
   my_modal_5.showModal();
+}
+
+const cartCounter=()=>{
+  const cartCounterId = document.getElementById('counter') ;
+  const itemCount = document.getElementById('itemCount');
+  cartCounterId.innerText = parseInt(count);
+  itemCount.innerText = parseInt(count);
+
 }
